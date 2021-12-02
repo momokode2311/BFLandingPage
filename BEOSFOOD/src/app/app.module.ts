19abd06c268923bc2258a673915295c382047a0b
MomoKode <72660955+momokode2311@users.noreply.github.com>
2021-12-02 22:28:28 +0700

    first touch

--- BEOSFOOD/src/app/app.module.ts
+++ BEOSFOOD/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent,
+    ShoppingCartComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule
---
+++ BEOSFOOD/src/app/base/IMFoodsBase.ts
@@ -0,0 +1,16 @@
+declare class IMFoodsBase {
+
+    // Mã sản phẩm
+    cat_cd :string;
+
+    cat_name: string;
+
+    describe: string;
+
+    unit_price: number;
+
+    isPromotion: boolean;
+
+    unit_price_prom: number;
+
+}
